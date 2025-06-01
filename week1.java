e6e3f7bf994da6c10718b1b10bc84151be4cfb19
pravallika <161574466+pravallika-badduri@users.noreply.github.com>
2025-06-01 13:33:58 +0530

    Add files via upload
    Week -1
    Task 1: Hello World Program
    Create a Java program to print "Hello, World!" on the console.

---
+++ week1.java
@@ -0,0 +1,11 @@
+// This is a basic Java program
+public class HelloWorld 
+{  
+    // A class named HelloWorld
+    public static void main(String[] args)
+     {
+         // The main method: the starting point of any Java program
+        System.out.println("Hello, World!");
+         // Prints "Hello, World!" to the console
+    }
+}
